bd6d3f2c69ed25a43b939723be6f35800e3f04dd
landaverdes <slandaverde307@gmail.com>
2020-11-01 19:10:23 -0500

    added code

---
+++ NumberedList/numListchanges.js
@@ -0,0 +1,11 @@
+var getLastname = function(people) {
+    return people.lastName;
+}
+
+d3.select("body")
+  .append("ol")
+  .selectAll("li")
+  .data(employees)
+  .enter()
+  .append("li")
+  .text(getLastname)
---
+++ PhotoBook/photosChanges.js
@@ -0,0 +1,10 @@
+var getPhoto = function(people) {
+    return people.photo;
+}
+
+d3.select("body")
+  .selectAll("img")
+  .data(employees)
+  .enter()
+  .append("img")
+  .attr("src",getPhoto);
